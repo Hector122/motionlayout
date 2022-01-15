1dc4af0e9c7cb96f4af2ddc7d97a43ea474702ce
Hector Castillo <h.castillo@gbh.com.do>
2022-01-15 13:46:00 -0400

    motion layout

--- motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step8Activity.kt
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step8Activity.kt
@@ -24,16 +24,24 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.appbar.AppBarLayout
 
 class Step8Activity : AppCompatActivity() {
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step8)
-
+        
         coordinateMotion()
     }
-
+    
     private fun coordinateMotion() {
         // TODO: set progress of MotionLayout based on an AppBarLayout.OnOffsetChangedListener
-
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+        
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+        
+        appBarLayout.addOnOffsetChangedListener(listener)
     }
 }
